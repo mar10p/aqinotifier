a0e6b29bdb159c535c6aa6e24c9a5195e02189c8
Martin Przyjazny <mprzyjazny@salesforce.com>
2021-08-29 20:46:48 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Dependencies
+- curl
+- [pup](https://github.com/EricChiang/pup) (available via OS X [Homebrew](https://brew.sh/))
+- [Prowl](https://www.prowlapp.com/) account
+- iPhone
+
+## Setup
+1. Set up your Prowl account
+2. Install the Prowl app on your phone
+3. Edit the config file
+4. Set up a cron to run this periodically (*/15 mins-ish or as desired)
---
+++ aqinotifier.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+MYPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source ${MYPATH}/aqinotifier.conf
+date >> "${LOG}"
+
+AQI=$(curl -s "${URL}" | pup 'p.aqi-value__value text{}' | xargs)
+
+if [ ! "${AQI}" ]; then
+  echo "FAILED to read AQI" >> "${LOG}"
+  curl -s -X POST \
+    -F apikey="${PROWLAPIKEY}" \
+    -F application="${APPNAME}" \
+    -F priority="${PRIORITY}" \
+    -F url="${URL}" \
+    -F event="Unable to get AQI" \
+    -F description="Was not able to read the AQI from iqair.com" \
+    "${PROWLURL}/add" >> "${LOG}"
+  exit
+fi
+
+if [ ! -s "${STATE}" ]; then
+  echo "${AQI}" > "${STATE}"
+fi
+
+STATE_DATA=$(cat "${STATE}")
+PREVIOUS=$(aqimap "${STATE_DATA}")
+CURRENT=$(aqimap "${AQI}")
+
+echo "OLD AQI=${STATE_DATA}, NOW AQI=${AQI}" >> "${LOG}"
+
+if [ "${PREVIOUS}" != "${CURRENT}" ]
+then
+  curl -s -X POST \
+    -F apikey="${PROWLAPIKEY}" \
+    -F application="${APPNAME}" \
+    -F priority="${PRIORITY}" \
+    -F url="${URL}" \
+    -F event="AQI Change Alert" \
+    -F description="AQI=${AQI}" \
+    "${PROWLURL}/add" >> "${LOG}"
+else
+  : # Do something else, if desired
+fi
+
+echo "${AQI}" > "${STATE}"
